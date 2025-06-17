55b3a31daf00906d3f7aaad9ab58111d77722eaf
ningluchang <ningluchang17@gmail.com>
2025-06-17 23:23:54 +0800

    新增分页和过滤查询

--- Author_Book/models.py
+++ Author_Book/models.py
@@ -10,7 +10,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author,related_name='books', on_delete=models.CASCADE)
     # 价格字段
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2, default=0)
 
     def __str__(self):
         return self.title
